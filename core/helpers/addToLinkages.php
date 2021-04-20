5adc2c21d2bb5dba35547f237403ddd7abeb96e5
atom-box <atom_box@hotmail.com>
2021-04-20 18:49:35 -0500

    Make random string URL without checking for collision with existing URL.

--- core/helpers/addToLinkages.php
+++ core/helpers/addToLinkages.php
@@ -3,15 +3,10 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-// die;
-// define('__ROOT__', dirname(dirname(__FILE__)));
-// require_once(__ROOT__.'/dbTransaction.php'); 
 require_once ('./core/helpers/dbTransaction.php');  // crasher
-echo '<h3>------a%*^%^$*%&$##-------</h3>'; 
 require_once('./core/config/dbconfig.inc.php');  // crasher
 
 // don't hit this code until after clicking submit form
-
 $user = USER;
 $password = SECRET;
 $database = NAMEOFDATABASE;  
@@ -25,24 +20,30 @@ $sessionValues =
 [
     'user_id'     => 999 ,
     'date'        => date('Y-m-d H:i:s', time()),
-    // 'date'        => gmstrftime('Y/m/d H:i:s'),
 ];
 
-// $session_id = $transaction->last_insert_id;
+$A_z = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*({?';
+$randomString = '';
+for ($i = 0; $i < 6; $i++) { 
+    $randomString .= $A_z[random_int(0, 63)]; 
+}
+$session_id = $transaction->last_insert_id;
+$longurl    =  'https://jangkajawa.blogspot.com/2021/04/apa-itu-merbot.html';
+$short      =  'https://tinyurl.com/' . $randomString;
 
-// $linkQuery = "insert into links (session_id, longurl, short) values (:session_id , :longurl , :short  )";
-// $linkValues = 
-// [
-//     'longurl' =>  'https://jangkajawa.blogspot.com/2021/04/apa-itu-merbot.html',
-//     'short' =>  'https://tinyurl.com/exp8s3n3',
-//     'session_id' => $session_id,
-// ];
+$linkQuery = "insert into links (session_id, longurl, short) values (:session_id , :longurl , :short  )";
+$linkValues = 
+[
+    'longurl' =>  $longurl,
+    'short' =>  $short,
+    'session_id' => $session_id,
+];
 
 // save the session, THEN ASK THE SQL WHICH SERIAL ID WAS IT?
 $transaction->insertQuery($sessionQuery, $sessionValues);
   
 
-// $transaction->insertQuery($linkQuery, $linkValues);
+$transaction->insertQuery($linkQuery, $linkValues);
 //  now that you know that serial id, write the linkl
 
 $transaction->startTransaction();
