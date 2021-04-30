259e6ee97954ff738895812ed88090cf2ef04dfd
Evan Genest <genest.1@osu.edu>
2021-04-30 11:07:18 -0500

    Delete phoenix.php

--- phoenix.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (hash_file('SHA384', 'composer-setup.php') === '$COMPOSER) { 
-    echo ('Installer verified'); 
-    } else { 
-        echo 'Installer corrupt'; unlink('composer-setup.php'); 
-    } echo PHP_EOL;
