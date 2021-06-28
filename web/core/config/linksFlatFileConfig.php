fb6782610b620ed6b0251aeccf09516d4f1e039b
atom-box <atom_box@hotmail.com>
2021-06-28 13:54:58 -0500

    Whitespace.

--- web/core/config/linksFlatFileConfig.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// do sync;    DON'T COMMIT
-// do sync;    DON'T COMMIT
-// do sync;    DON'T COMMIT
-
-const APACHE_FLATFILE = '/home/evan/projects/do-not-commit/shortener.txt';
-const CUSTOMER_FACING_STUB = 'https://pikl.us/';
