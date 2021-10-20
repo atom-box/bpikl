48b71a3e5448ed68d798d8d42bee313914e8a403
atom-box <atom_box@hotmail.com>
2021-10-19 20:42:59 -0500

    Prevent entry of MEGA as leading URL.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-## Assignment
+## Goal
 
 Create a small web app which creates short links (like bit.ly)
 
-## Requirements
+## Story Requirements
 
 There should be three “actions” a user can take:
 *  Main page (/)  - contains a form where long URLs can be input and submitted. Form redirects to the info page on success. Must have validation. 
 *  Short link info page (/view/abcd123) – shows information about the shortened URL.
 *  Redirect link (/abcd123) – looks up the full URL and redirects to it.
+
+## See it live
+
+This is running now at [https://pikl.us](https://pikl.us) 
+
+## Version
+
+This supercedes the earlier Pikl and APikl repositories
--- web/templates/formCard.php
+++ web/templates/formCard.php
@@ -1,10 +1,29 @@
+<!-- Temporary Spam checker -->
+<!-- TODO make a proper script, not here-->
+<script>
+    console.log('hi ho bobby...');
+//     const longURL = document.getElementById("long");
+
+// longURL.addEventListener("input", function (event) {
+//   if (longURL.validity.typeMismatch) {
+//     longURL.setCustomValidity("I am expecting an e-mail address!");
+//   } else {
+//     longURL.setCustomValidity("");
+//   }
+// });
+
+
+</script>
+
+
 <div class="card border-primary mb-4" style="max-width: 55rem;">
     <div class="card-body">
         <p class="card-text">
         <form class="form-inline" method="post" action="linksPage.php">
             <div class="form-group">
             <label >Try it now: </label>
-                <input type="url" required name="longurl" class="form-control" id="long" placeholder="Long URL">
+                <input type="url" name="longurl" required 
+           pattern="........[^m][^e][^g][^a].*"  class="form-control" id="long" placeholder="Long URL">
             </div>
             <button type="submit" class="btn btn-info">Shorten it!</button>
         </form>
