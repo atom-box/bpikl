3d6f3d4dce44ab8a6f4896771008f9820fd3a6b0
Evan Genest <atom_box@hotmail.com>
2023-12-05 15:45:36 -0600

    Add MTM container for mouseover experiments to top.php

--- web/templates/top.php
+++ web/templates/top.php
@@ -34,10 +34,12 @@
 <!-- End Matomo Code -->
 <!-- Matomo Tag Manager -->
 <script>
-var _mtm = window._mtm = window._mtm || [];
-_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-g.async=true; g.src='https://littlefurnace.com/matomo/js/container_b18KuBVf.js'; s.parentNode.insertBefore(g,s);
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  (function() {
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://littlefurnace.com/matomo/js/container_jOUsVzXH.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
 <!-- End Matomo Tag Manager -->
 
